568632ceccec98e2efaf43026ba33fe868265fe8
愛知巧規 <prosenyou123@gmail.com>
2018-12-11 15:19:44 +0900

    同期処理と非同期処理の比較。あとフォルダ整理した

---
+++ node-js/同期通信と非同期通信比較/readfile-sync.js
@@ -0,0 +1,15 @@
+//同期処理と非同期処理の使い勝手を比較してみる
+//それぞれテキストファイルを読み込んでコンソールに表示している
+
+const fs = require('fs')
+
+// --- 同期的にファイルを読み込む --- (※1)
+const data = fs.readFileSync('kakugen.txt', 'utf-8')
+console.log(data)
+
+// --- 非同期でファイルを読み込む --- (※2)
+fs.readFile('kakugen.txt', 'utf-8', readHandler)
+// 読み込みが完了したときの処理
+function readHandler (err, data) {
+  console.log(data)
+}

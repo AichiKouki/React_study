0f8a021a4bcc58a5f4b8c8826b4fab3b8be9f96e
愛知巧規 <prosenyou123@gmail.com>
2018-12-13 15:44:26 +0900

    ES2015のimport/exportを使ってライブラリの関数定義とその読み込みを行った

---
+++ node-js/モジュール機構/ES2015のimport:export使う/es2015io/dest/calctest.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.add = add;
+exports.mul = mul;
+//ES2015のimport/export
+
+// 外部に公開する関数を定義
+function add(a, b) {
+  return a + b;
+}
+function mul(a, b) {
+  return a * b;
+}
---
+++ node-js/モジュール機構/ES2015のimport:export使う/es2015io/src/calctest.js
@@ -0,0 +1,10 @@
+//ES2015のimport/export
+
+// 外部に公開する関数を定義
+//functionの前にexportを記述することによって、その関数を外部に公開することを宣言
+export function add (a, b) {
+  return a + b
+}
+export function mul (a, b) {
+  return a * b
+}

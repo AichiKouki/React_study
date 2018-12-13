0e18763cc21b40dbd7c1d5fd0496f4617f358e5a
愛知巧規 <prosenyou123@gmail.com>
2018-12-13 16:19:40 +0900

    モジュールを使うにあたってデフォルト要素を指定する方法

---
+++ node-js/モジュール機構/ES2015のimport:export使う/デフォルト要素を指定/src/kakezan.js
@@ -0,0 +1,10 @@
+//デフォルト要素を指定
+//モジュールの中で出力する要素が一つの場合は、defaultを指定した方がやりやすい
+
+// 関数を定義
+function kakezan(a, b) {
+  return a * b;
+}
+// kakezanをデフォルトで公開
+export default kakezan;
+
---
+++ node-js/モジュール機構/ES2015のimport:export使う/デフォルト要素を指定/src/main.js
@@ -0,0 +1,9 @@
+//デフォルト要素を指定した方法
+//kakezan.jsで関数が一つしかないので、以下のようにそのまま簡単に使える
+
+// kakezanモジュールを取り込む
+import kakezan from './kakezan';
+// kakezan関数を利用
+const v = kakezan(2, 3);
+console.log(v);
+

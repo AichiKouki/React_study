ff14e52f6fad32100c7a7f3f02a955fe20c9ae01
愛知巧規 <prosenyou123@gmail.com>
2018-12-13 15:50:16 +0900

    さっきのファイルにコメントを追記した

--- node-js/モジュール機構/ES2015のimport:export使う/es2015io/src/main.js
+++ node-js/モジュール機構/ES2015のimport:export使う/es2015io/src/main.js
@@ -1,5 +1,8 @@
 // 自作の計算モジュール「calctest.js」を取り込む
-import {add, mul} from './calctest.js'
+import {add, mul} from './calctest.js' //add関数とmul関数を指定
+
+//指定したファイル(モジュール)のすべての要素を取り込む場合は、
+//import * as ct from './calctest.js'; って感じで「*」は全部で、ctは勝手につけた名前で、「ct.add()」って感じでアクセスできるようになる
 
 // 取り込んだ関数を利用する
 console.log(add(2, 3))

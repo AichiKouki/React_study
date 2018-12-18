bdf559da8e8b42e62007c3056dfb51f8c51e4a4c
愛知巧規 <prosenyou123@gmail.com>
2018-12-18 16:11:44 +0900

    Virtual DOMで、差分で最小限の部分だけ更新。その機能を使って時計機能を作った

--- node-js/モジュール機構/ES2015のimport:export使う/デフォルト要素を指定/dest/kakezan.js
+++ node-js/モジュール機構/ES2015のimport:export使う/デフォルト要素を指定/dest/kakezan.js
@@ -3,6 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+//デフォルト要素を指定
+//モジュールの中で出力する要素が一つの場合は、defaultを指定した方がやりやすい
+
 // 関数を定義
 function kakezan(a, b) {
   return a * b;
--- node-js/モジュール機構/ES2015のimport:export使う/デフォルト要素を指定/dest/main.js
+++ node-js/モジュール機構/ES2015のimport:export使う/デフォルト要素を指定/dest/main.js
@@ -7,6 +7,9 @@ var _kakezan2 = _interopRequireDefault(_kakezan);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // kakezan関数を利用
-var v = (0, _kakezan2.default)(2, 3); // kakezanモジュールを取り込む
+var v = (0, _kakezan2.default)(2, 3); //デフォルト要素を指定した方法
+//kakezan.jsで関数が一つしかないので、以下のようにそのまま簡単に使える
+
+// kakezanモジュールを取り込む
 
 console.log(v);

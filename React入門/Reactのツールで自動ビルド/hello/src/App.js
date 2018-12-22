1b88e715240af7045a3aba24e41cdb8fbc41ac5a
AichiKouki <b5p31001@shonan.bunkyo.ac.jp>
2018-12-22 10:36:53 +0000

    自動ビルドしたファイル追加

---
+++ React入門/Reactのツールで自動ビルド/hello/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+	<h2>仮想環境でReact実行中。文字列を表示しているよ。まなったん推し</h2>
+      </div>
+    );
+  }
+}
+
+export default App;

4faa1a96b73e50e514e3c7b3b940cf16c289dbbb
AichiKouki <b5p31001@shonan.bunkyo.ac.jp>
2019-01-16 04:53:13 +0000

    Reactにおけるフォーム部品を扱う(テキストエリア)

--- Reactコンポーネントの作成/Reactにおけるフォーム部品の扱い方/forms/src/index.js
+++ Reactコンポーネントの作成/Reactにおけるフォーム部品の扱い方/forms/src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import TextForm from './text';
 import CBoxForm from './cbox';
+import TextAreaForm from './textarea';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<CBoxForm />, document.getElementById('root'));
+ReactDOM.render(<TextAreaForm />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

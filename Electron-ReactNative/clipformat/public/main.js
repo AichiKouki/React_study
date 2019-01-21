be07626754dc8a4798250a6a6274e015cb2fcc74
AichiKouki <b5p31001@shonan.bunkyo.ac.jp>
2019-01-21 07:35:54 +0000

    さっきのプロジェクトのコメント追加

--- Electron-ReactNative/clipformat/public/main.js
+++ Electron-ReactNative/clipformat/public/main.js
@@ -1,5 +1,5 @@
 //Electronの実行に必要なモジュールを取り込む
-const { app, BrowserWindow } = require('electron') //BrowserWindowに読み込むURLを設定する
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
 //Electronのライフサイクルを定義
@@ -9,8 +9,8 @@ let mainWindow //メインウィンドウを表す変数
 function createWindow() {
   mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
-  //publicの中ではないその上の階層のpackage.jsonに、環境変数 ELECTRON_START_URL が定義されていればそれを使う。なければ index.html を使う。
-  const startUrl = process.env.ELECTRON_START_URL || url.format({//読み込むコンテンツを指定。
+  // 環境変数 ELECTRON_START_URL が定義されていればそれを使う。なければ index.html を使う。
+  const startUrl = process.env.ELECTRON_START_URL || url.format({//読み込むコンテンツを指定。index.htmlを読み込む
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true

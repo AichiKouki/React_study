e444768f9486c28a6b1aaf1d2f91ed4add8ba1e0
AichiKouki <b5p31001@shonan.bunkyo.ac.jp>
2019-01-23 06:00:09 +0000

    さっきの書き込みプログラムで、固定の文字列で投稿と、入力した文字列で投稿を別々のプログラムに分けた

--- Electron-ReactNative/mastodon_cli/src/post_from_terminal.js
+++ Electron-ReactNative/mastodon_cli/src/post_from_terminal.js
@@ -2,6 +2,8 @@ const Mastodon = require('mastodon-api')
 const fs = require('fs')
 const path = require('path')
 const instanceUri = 'https://pawoo.net'
+const readlineSync = require('readline-sync')
+
 
 // ファイルから情報を読み込む
 const token = fs.readFileSync(path.join(__dirname, '../token.json'))
@@ -13,9 +15,12 @@ const M = new Mastodon({
   api_url: instanceUri + '/api/v1/'
 })
 
+// コマンドラインから書き込みたい文字を入力
+const sentence = readlineSync.question('書き込みたい文章を入力してね: ') //質問の表示と入力した値を取得
+
 // 発言(Toot)する 
 M.post('statuses',  //3_get_timelineと違う点はここから。
-  {status: 'TEST TEST TEST by cli'}, //「TEST TEST TEST TEST by cli」が書き込み内容
+  {status: sentence}, //「TEST TEST TEST TEST by cli」が書き込み内容
   (err, data, res) => {
     if (err) {
       console.error(err)

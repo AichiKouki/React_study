6798a31a904dca0a5987b3a15be5f174b06c921d
AichiKouki <b5p31001@shonan.bunkyo.ac.jp>
2019-03-08 08:13:58 +0000

    自分のSNS作成

---
+++ spaのためのフレームワーク/sns/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route, Switch
+} from 'react-router-dom'
+import SNSUsers from './sns_users'
+import SNSTimeline from './sns_timeline'
+import SNSLogin from './sns_login'
+//React Routerを利用して、URLに応じて表示するコンポーネントを分岐する。
+const SNSApp = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route path='/users' component={SNSUsers} />
+        <Route path='/timeline' component={SNSTimeline} />
+        <Route path='/login' component={SNSLogin} />
+        <Route component={SNSLogin} />
+      </Switch>
+    </div>
+  </Router>
+)
+
+// DOMにメインコンポーネントを書き込む
+ReactDOM.render(
+  <SNSApp />,
+  document.getElementById('root'))
+

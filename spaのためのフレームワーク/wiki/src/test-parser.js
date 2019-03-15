4e6543efc7cb06dcc007d39a589aace824c26173
AichiKouki <prosenyou123@gmail.com>
2019-03-15 04:53:01 +0000

    フォルダ整理

--- spaのためのフレームワーク/wiki/src/test-parser.js
+++
@@ -1,8 +0,0 @@
-// パーサを取り込む
-const WikiParser = require('./wiki_parser.js')
-// ソースコードをパース
-const src = '*title\n\n-list1\n-list2\n\nhoge'
-const nodes = WikiParser.parse(src)
-console.log(nodes)
-
-
